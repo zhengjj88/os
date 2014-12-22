b5031ad76d7a6df8a3eee9e2366d10d0e6494d33
david <junjiezheng168@outlook.com>
2014-12-22 08:49:45 +0800

    update app.ts to fix bug of page query

--- kmapp/app.ts
+++ kmapp/app.ts
@@ -92,7 +92,7 @@ app.get('/api/v1/user', function(req, res){
       pageSize = 20;
     }
     if(pageNumber == undefined) {
-      pageNumber == 0;
+      pageNumber = 0;
     }
     var page = new db.Page(pageNumber, pageSize);
     db.findUsers(null, page, function(users) {
